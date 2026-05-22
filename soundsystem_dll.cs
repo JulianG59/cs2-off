d86b9793a6359743ce02f3ca470c49170157c01e
JulianG59 <77183204+JulianG59@users.noreply.github.com>
2026-05-22 03:49:19 +0200

    Add files via upload

--- soundsystem_dll.cs
+++ soundsystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-21 03:34:54.542047400 UTC
+// 2026-05-22 01:48:21.207584600 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: soundsystem.dll
--- steamaudio_dll.zig
+++ steamaudio_dll.zig
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-21 03:34:54.542047400 UTC
+// 2026-05-22 01:48:21.207584600 UTC
 
 pub const cs2_dumper = struct {
     pub const schemas = struct {
