10ddb8ca0ca06b0860899f26b6865486dd7e64c8
JulianG59 <julianossa2406@gmail.com>
2026-06-16 15:25:59 +0200

    Sync local files with GitHub

--- upload.py
+++ upload.py
@@ -1,22 +1,3 @@
-cd "C:\Users\Memez\Desktop\dump\output"
-
-git init
-git branch -M main
-git remote add origin https://github.com/JulianG59/cs2-off.git
-
-git pull origin main --allow-unrelated-histories
-
-git add .
-git commit -m "Upload files"
-
-git push origin main
-
-
-
-git config --global user.name "JulianG59"
-git config --global user.email ""
-
-
 cd "C:\Users\Memez\Desktop\dump"
 
 git config --global user.name "JulianG59"
