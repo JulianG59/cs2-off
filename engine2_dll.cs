0755e50a52a62b8246811089c5f5ef17ae02d621
JulianG59 <julianossa2406@gmail.com>
2026-07-01 04:59:38 +0200

    Update 2026-07-01 04:59:38

--- engine2_dll.cs
+++ engine2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-06-16 13:20:13.076450400 UTC
+// 2026-07-01 02:59:27.539920500 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: engine2.dll
--- host_dll.rs
+++ host_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-06-16 13:20:13.076450400 UTC
+// 2026-07-01 02:59:27.539920500 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
