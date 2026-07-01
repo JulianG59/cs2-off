853fb2d7cf7aca41f3572dca33097338bd7de07b
JulianG59 <julianossa2406@gmail.com>
2026-07-01 19:15:34 +0200

    Update 2026-07-01 19:15:34

--- materialsystem2_dll.rs
+++ materialsystem2_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-01 08:45:34.761754800 UTC
+// 2026-07-01 17:15:32.658807300 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- networksystem_dll.cs
+++ networksystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-01 08:45:34.761754800 UTC
+// 2026-07-01 17:15:32.658807300 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: networksystem.dll
