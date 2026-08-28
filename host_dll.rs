2b445f77f460934782ec7ce25ec73b0363245a11
JulianG59 <julianossa2406@gmail.com>
2026-08-28 23:40:24 +0200

    Update 2026-08-28 23:40:24

--- host_dll.rs
+++ host_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-26 21:18:26.831337600 UTC
+// 2026-08-28 21:40:21.898131500 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
